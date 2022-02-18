b3ece014aacec93b3ef3642a8c204ff43eb4b991
Xuemin Guan <xueminguan@Xuemins-MacBook-Pro.local>
2022-02-18 18:37:19 +0000

    XG adding css

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,17 @@
-import {LiveReload, Outlet} from "remix";
+import { Links, LiveReload, Outlet } from "remix";
 
 export default function App() {
-  return (
-      <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <title>Remix: So great, it's funny!</title>
-      </head>
-      <body>
-      {process.env.NODE_ENV === "development" ? (
-          <LiveReload />
-      ) : null}
-      <Outlet />
-      </body>
-      </html>
-  );
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8" />
+            <title>Remix: So great, it's funny!</title>
+            <Links />
+        </head>
+        <body>
+        <Outlet />
+        <LiveReload />
+        </body>
+        </html>
+    );
 }
